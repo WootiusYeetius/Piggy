1961bae4ac1f1f55a2fbfbf5085cb11821097e54
WootiusYeetius <100741850+WootiusYeetius@users.noreply.github.com>
2023-04-27 18:50:13 +0000

    safetests1

--- student.py
+++ student.py
@@ -121,9 +121,11 @@ class Piggy(PiggyParent):
 
     def scan(self):
         """Sweep the servo and populate the scan_data dictionary"""
-        for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 3):
-            self.servo(angle)
-            self.scan_data[angle] = self.read_distance()
+      for angle in range(self.MIDPOINT-350, self.MIDPOINT+350, 3):
+        self.servo(angle)
+        self.scan_data[angle] = self.read_distance()
+      print(self.scan_data)
+        
 
     def obstacle_count(self):
         """Does a 360 scan and returns the number of obstacles it sees"""
